a82e7c730f384fe07ed8151a75d5905b2e797cdd
Hasan <hasan.error.code@gmail.com>
2024-01-14 10:30:47 +0600

    Data Structure

---
+++ For.py
@@ -0,0 +1,7 @@
+for i in range(10, 20, 2):
+    print(i)
+
+a = 10
+while a < 20:
+    print("Value a: ", a)
+    a += 1
---
+++ List.py
@@ -0,0 +1,10 @@
+lis = ["Hasan", 23, "CSE", "Daffodil"]
+print("Befor Edit", lis)
+lis[1] = 24
+print("After Edit:", lis)
+
+lis.append("Habiba")
+print("Append: ", lis)
+
+lis.insert(0, "Database")
+print(lis)
