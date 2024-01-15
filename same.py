751f40f11cc3c2196fc22a0b2bd05f6af0896b16
Hasan <hasan.error.code@gmail.com>
2024-01-15 22:15:09 +0600

    Error

---
+++ same.py
@@ -0,0 +1,14 @@
+# user = "1 1"
+# users = user.split()
+# l = len(user)
+# print(l)
+# for i in users:
+#     u = i
+    
+#     for j in range(l): 
+#         o = j
+
+#         if u == o:
+#             continue
+#         else:
+#             print("error")
