3c22ae9f641b4220a35a39bcc201bba1938deb5b
Hasan <hasan.error.code@gmail.com>
2024-01-18 15:52:57 +0600

    UAR

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+user = {"1": "Python", "2":"Django", "3": "pip", "4": "git"}
+user["2"] = "Hasan"
+#Update Dictionary
+print(user)
+
+#Add item in Dictionary
+
+user["5"] = "Hello"
+print(user)
+
+#remove item
+user.pop("3")
+print(user)
